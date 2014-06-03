a1e84753245e905c7ef48a4772973bc2aa0a1f23
Igor Chaika <kp580bm1@gmail.com>
2014-06-03 14:41:14 +0300

    Showing popup image on image click.

--- imageview.js
+++ imageview.js
@@ -1,10 +1,21 @@
+function show_gallery(image_source) {
+	document.getElementById("black_overlay").style.display = "block";
+	var gallery = document.getElementById("popup_gallery");
+	gallery.style.display = "block";
+	var image = document.getElementById("gallery_image");
+	image.src = image_source;
+}
+function hide_gallery() {
+	document.getElementById("black_overlay").style.display = "none";
+	document.getElementById("popup_gallery").style.display = "none";
+}
 window.onload = function() {
 	var list = document.getElementsByTagName("a");
 	for (var i = 0; i < list.length; ++i) {
 		var a = list[i];
 		if(a.getElementsByTagName("img").length > 0) {
 			a.onclick = function() {
-				window.alert("HELLO");
+				show_gallery(this.href);
 				return false;
 			}
 		}
