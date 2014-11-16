13dd10d7aaf392594fa1dd1d26874c287a93e3a6
Igor Chaika <kp580bm1@gmail.com>
2014-11-16 10:57:35 +0200

    Move convenient CV script usage: different invokation for each CV file, usage text, checking filename formats.

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 THUMBS = $(shell grep -ahro --exclude _new_post.sh --exclude Makefile --exclude *swp --exclude-dir _site --exclude-dir .git 'thumbs/[^ "]\+')
+CV = cv.en.md cv.ru.md
 
 all: cv $(THUMBS)
 	jekyll --pygments
 
-cv:
-	python3 _cv.py
+cv: $(CV)
+
+cv.%.md: _cv.%.yml
+	python3 _cv.py $<
 
 thumbs/%: images/%
 	convert $^ -thumbnail '200x200>' $@
--- _cv.py
+++ _cv.py
@@ -1,5 +1,19 @@
 from collections import namedtuple
 import yaml
+import re
+import sys
+
+USAGE = """Usage: python3 _cv.py <cv file> ...
+
+CV filename should be in format `_cv.XX.yml`,
+where `XX` is two-letter language index.
+Output will be stored in `cv.XX.md`
+"""
+
+def usage(error_message):
+	if error_message:
+		print(error_message)
+	print(USAGE)
 
 def to_struct(data):
 	if isinstance(data, dict):
@@ -9,20 +23,26 @@ def to_struct(data):
 	return data
 
 def main():
+	if len(sys.argv) < 2:
+		usage()
+		return 1
+
 	with open("_cv.md", "r") as f:
 		template = f.read()
+	for filename in sys.argv[1:]:
+		m = re.match(r'_cv.([a-z]{2}).yml', filename)
+		if not m:
+			usage("Wrong format of CV file name: " + filename)
+			return 1
+		lang = m.group(1)
+		dest_filename = "cv.{0}.md".format(lang)
 
-	with open("_cv.en.yml", "r") as f:
-		data = yaml.load(f)
-	data = to_struct(data)
-	with open("cv.en.md", "w") as f:
-		f.write(template.format(**(data.__dict__)))
-
-	with open("_cv.ru.yml", "r") as f:
-		data = yaml.load(f)
-	data = to_struct(data)
-	with open("cv.ru.md", "w") as f:
-		f.write(template.format(**(data.__dict__)))
+		with open(filename, "r") as f:
+			data = yaml.load(f)
+		data = to_struct(data)
+		with open(dest_filename, "w") as f:
+			f.write(template.format(**(data.__dict__)))
 
 if __name__ == "__main__":
-	main()
+	retcode = main()
+	sys.exit(retcode)
